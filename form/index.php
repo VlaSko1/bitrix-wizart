a71729862e728a6c49eaab1bce04fbea76d9ee53
VlaSko1 <vizion84@yandex.ru>
2022-06-16 23:44:01 +0300

    Add init.php and change .gitignore

---
+++ form/index.php
@@ -0,0 +1,7 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Form");
+?>
+Hello World first
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ local/templates/form/description.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$arTemplate = [
+  'NAME' => 'Шаблон wizart',
+  'DESCRIPTION' => 'Тестовое задание от компании Wizart',
+  'SORT' => 300,
+  'TYPE' => '',
+];
