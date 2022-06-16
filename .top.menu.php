76b1c75b7eeb7c9781b9135963b0490a5a7810a6
VlaSko1 <vizion84@yandex.ru>
2022-06-16 19:21:15 +0300

    Initial git

---
+++ .top.menu.php
@@ -0,0 +1,32 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Главная", 
+		"/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Профиль", 
+		"/personal/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Общение", 
+		"/communication/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Контент", 
+		"/content/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
